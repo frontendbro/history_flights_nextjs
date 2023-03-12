d22b982de323310d3c66b9ff3d28623e23aab50c
Semyon A. Zadorozhniy <semyon@codedesign.ru>
2023-03-12 13:53:45 +0700

    minor fix

--- components/Card/index.js
+++ components/Card/index.js
@@ -36,14 +36,10 @@ const Card = ({ launch, updateStatusState, star }) => {
               className={styles["card-rocket-title__icon"]}
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
                 d="M4.00008 0H12.0001V21.5858L15.7072 25.2929L14.293 26.7071L10.0001 22.4142V2H6.00008V22.4142L1.70718 26.7071L0.292969 25.2929L4.00008 21.5858V0Z"
                 fill="#EBEBEB"
               />
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
                 d="M7.00012 30L7.00012 25L9.00012 25L9.00012 30L7.00012 30Z"
                 fill="#EBEBEB"
               />
